50a12ea9236e24a10619a86b6c313802dc6f3d9f
Nir Orman <nirorman@gmail.com>
2016-12-24 19:08:24 +0200

    move the logic of the main into a new class: FullJsonFetcher

--- src/main/java/com/nirorman/zendesk/Main.java
+++ src/main/java/com/nirorman/zendesk/Main.java
@@ -1,19 +1,11 @@
-package com.nirorman.zendesk;
+package main.java.com.nirorman.zendesk;
 
-import com.nirorman.zendesk.exceptions.CreateHtmlFileException;
-import com.nirorman.zendesk.exceptions.FetchJsonException;
-import com.nirorman.zendesk.impl.HtmlCreatorImpl;
-import com.nirorman.zendesk.impl.JsonFetcherImpl;
-import org.json.simple.JSONObject;
+import main.java.com.nirorman.zendesk.api.FullJsonFetcher;
+import main.java.com.nirorman.zendesk.impl.FullJsonFetcherImpl;
+import static main.java.com.nirorman.zendesk.utils.FileNamesUtil.USAGE_MESSAGE;
 
-import static com.nirorman.zendesk.utils.FileNamesUtil.CATEGORIES;
-import static com.nirorman.zendesk.utils.FileNamesUtil.URL_TEMPLATE;
 
 public class Main {
-    private static final String USAGE_MESSAGE = "Usage: \n" +
-            "1. the base Zendesk account host (e.g. smartling.zendesk.com). \n" +
-            "2. the target local directory on the computer running the program (e.g. c:\\temp)\n";
-
 
     public static void main(String[] args) throws Exception{
         if (args.length != 2){
@@ -23,23 +15,7 @@ public class Main {
         final String accountHost = args[0];
         final String targetDirPath = args[1];
 
-        try {
-            String url = String.format(URL_TEMPLATE, accountHost, CATEGORIES);
-            JsonFetcherImpl fetcher = new JsonFetcherImpl(url);
-            JSONObject response = fetcher.fetchJson();
-
-            HtmlCreatorImpl htmlCreator = new HtmlCreatorImpl(targetDirPath);
-            htmlCreator.createHtmlFile(response);
-        }
-        catch (FetchJsonException e ){
-            System.out.println(String.format("Failed to fetch Json: %s" , e.getMessage()));
-        }
-        catch(CreateHtmlFileException e)
-        {
-            System.out.println(String.format("failed to read HTML file: %s" , e.getMessage()));
-        }
-        catch (Exception e) {
-            System.out.println(String.format("Unexpected error: %s" , e.getMessage()));
-        }
+        FullJsonFetcher fetcher = new FullJsonFetcherImpl(accountHost, targetDirPath);
+        fetcher.fetch();
     }
 }
