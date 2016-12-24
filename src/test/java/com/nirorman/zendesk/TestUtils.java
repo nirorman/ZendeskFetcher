9bd043c9d70074d81b946604086b7b3b27cf2f69
Nir Orman <nirorman@gmail.com>
2016-12-24 19:13:03 +0200

    move the categories json exmple into the test folder

---
+++ src/test/java/com/nirorman/zendesk/TestUtils.java
@@ -0,0 +1,27 @@
+package test.java.com.nirorman.zendesk;
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import static main.java.com.nirorman.zendesk.utils.FileNamesUtil.CATEGORIES_JSON;
+
+/**
+ * Created by ormann on 14/12/2016.
+ */
+ class TestUtils {
+
+    static JsonNode mockCategoriesJson() {
+        JsonNode node = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        File file = new File(TestUtils.class.
+                getResource(CATEGORIES_JSON).getPath());
+        try {
+            node = objectMapper.readValue(file, JsonNode.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return node;
+    }
+}
