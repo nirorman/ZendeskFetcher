d5a3fcb0210d5b38f866571d24cf6acc44ecbe54
Nir Orman <nirorman@gmail.com>
2016-12-25 12:59:00 +0200

    add ability to add pagination. add file for testing the output created by running full page fetcher with mockito

--- src/main/java/com/nirorman/zendesk/utils/FileNamesUtil.java
+++ src/main/java/com/nirorman/zendesk/utils/FileNamesUtil.java
@@ -8,8 +8,9 @@ public class FileNamesUtil {
     public static final String CATEGORIES_JSON = "/categories.json";
     public static final String TARGET_FILE_NAME = "CategoryLinks.html";
     public static final String ACCOUNT_HOST = "smartling.zendesk.com";
+    public static final String TEST_HOST = "nirorman.com";
     public static final String CATEGORIES = "categories";
-    public static final String URL_TEMPLATE = "https://%s/api/v2/help_center/%s.json%s";
+    public static final String URL_TEMPLATE = "https://%s/api/v2/help_center/%s.json%s%s";
     public static final String SORT_RESULTS = "?sort_by=updated_at&sort_order=asc";// reduces pagination risks
     public static final String TEMPLATE_FILE_NAME = "template.html";
     public static final String SEPARATOR = "/";
@@ -17,5 +18,6 @@ public class FileNamesUtil {
     public static final String USAGE_MESSAGE = "Usage: \n" +
             "1. the base Zendesk account host (e.g. smartling.zendesk.com). \n" +
             "2. the target local directory on the computer running the program (e.g. c:\\temp)\n";
+    public static final String MOCK_CATEGORIES_FILE_NAME = "mockCategories.html";
 }
 
