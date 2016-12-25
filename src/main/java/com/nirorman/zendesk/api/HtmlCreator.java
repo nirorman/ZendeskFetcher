b98a666863098c490b71c79bb4b274135aa2d583
Nir Orman <nirorman@gmail.com>
2016-12-25 13:03:46 +0200

    space out the interface

--- src/main/java/com/nirorman/zendesk/api/HtmlCreator.java
+++ src/main/java/com/nirorman/zendesk/api/HtmlCreator.java
@@ -6,7 +6,10 @@ import com.fasterxml.jackson.databind.JsonNode;
  * Created by ormann on 14/12/2016.
  */
 public interface HtmlCreator {
+
     void createHtmlFile() throws Exception;
+
     String getHtmlTargetFilePath();
+
     void addJsonNodeToHtmlBody(JsonNode jsonNode);
 }
