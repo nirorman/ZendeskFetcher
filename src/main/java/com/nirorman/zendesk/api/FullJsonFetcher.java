ff2fe1cb4a51e160c197adb3db91fcc788da31b7
Nir Orman <nirorman@gmail.com>
2016-12-25 13:00:17 +0200

    add ability to add pagination. add file for testing the output created by running full page fetcher with mockito

--- src/main/java/com/nirorman/zendesk/api/FullJsonFetcher.java
+++ src/main/java/com/nirorman/zendesk/api/FullJsonFetcher.java
@@ -1,9 +1,23 @@
 package main.java.com.nirorman.zendesk.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.List;
+
 /**
  * Created by ormann on 21/12/2016.
  */
 public interface FullJsonFetcher
 {
+    void fetch(String pagination);
+
     void fetch();
+
+    SinglePageFetcherService getSinglePageFetcherService();
+
+    void setSinglePageFetcherService(SinglePageFetcherService singlePageFetcherService);
+
+    List<JsonNode> getJsonNodes();
+
+    void setJsonNodes(List<JsonNode> nodes);
 }
