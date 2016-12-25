7a043cba79f3bd6e0e078fdffa29d6bbfc818d12
Nir Orman <nirorman@gmail.com>
2016-12-25 12:57:44 +0200

    add Url exception

--- src/main/java/com/nirorman/zendesk/exceptions/CreateHtmlFileException.java
+++ src/main/java/com/nirorman/zendesk/exceptions/CreateHtmlFileException.java
@@ -6,7 +6,9 @@ import java.io.IOException;
  * Created by ormann on 14/12/2016.
  */
 public class CreateHtmlFileException extends IOException {
+
     public CreateHtmlFileException(String message) {
         super(message);
     }
+
 }
---
+++ src/main/java/com/nirorman/zendesk/exceptions/UrlException.java
@@ -0,0 +1,12 @@
+package main.java.com.nirorman.zendesk.exceptions;
+
+
+/**
+ * Created by ormann on 25/12/2016.
+ */
+public class UrlException extends Exception {
+
+    public UrlException(String message) {
+        super(message);
+    }
+}
